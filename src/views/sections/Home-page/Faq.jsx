6b4b41c6beaeb7be93a179b5c7c5005d1a2a9060
Gustav Ivering <gustav.ivering@live.se>
2024-03-04 14:34:09 +0100

    Accordion group is working again

--- src/views/sections/Home-page/Faq.jsx
+++ src/views/sections/Home-page/Faq.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 import PhoneIcon from '../../../images/icons/Group_phone.svg'
 import RightArrowBlue from '../../../images/icons/icon-r_arrow.right-blue.svg'
 import MessageIcon from '../../../images/icons/icon_message.svg'
 import RightArrowGreen from '../../../images/icons/icon-r_greenArrow.svg'
+
+const AccordionItem = ({ title, content }) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const handleToggle = () => {
+      setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="card">
+          <div className="heading">
+            <button onClick={handleToggle}>
+              <h6>{title}</h6>
+            </button>
+            <i className={`fa-regular ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+          </div>
+          <div className={`card-body ${isOpen ? 'd-block' : 'd-none'}`}>
+            <p>{content}</p>
+          </div>
+        </div>
+      );
+    };
 
 const Faq = () => {
   return (
@@ -26,99 +48,50 @@ const Faq = () => {
                         </div>
                     </div>
                     <div className="accordion-group">
-                        <div className="card">
-                            <div className="heading">
-                                <button data-toggle="collapse" data-toggle-target="card-body-1">
-                                    <h6>Is any of my personal information stored in the App?</h6></button>
-                                <i className="fa-regular fa-chevron-down"></i>
-                            </div>
-                            <div id="card-body-1" className="card-body d-none">
-                                <p>
-                                    Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
-                                    Viverra aliquam arcu, viverra et, cursus. 
-                                    Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
-                                    Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
-                                    Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="heading">
-                                <button data-toggle="collapse" data-toggle-target="card-body-2"><h6>What formats can I download my transaction history in?</h6></button>
-                                <i className="fa-regular fa-chevron-down"></i>
-                            </div>
-                            <div id="card-body-2" className="card-body d-none">
-                                <p>
-                                    Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
-                                    Viverra aliquam arcu, viverra et, cursus. 
-                                    Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
-                                    Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
-                                    Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.    
-                                </p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="heading">
-                                <button data-toggle="collapse" data-toggle-target="card-body-3"><h6>Can I schedule future transfers?</h6></button>
-                                <i className="fa-regular fa-chevron-down"></i>
-                            </div>
-                            <div id="card-body-3" className="card-body d-none">
-                                <p>
-                                    Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
-                                    Viverra aliquam arcu, viverra et, cursus. 
-                                    Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
-                                    Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
-                                    Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.    
-                                </p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="heading">
-                                <button data-toggle="collapse" data-toggle-target="card-body-4"><h6>When can I use Banking App services?</h6></button>
-                                <i className="fa-regular fa-chevron-down"></i>
-                            </div>
-                            <div id="card-body-4" className="card-body d-none">
-                                <p>
-                                    Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
-                                    Viverra aliquam arcu, viverra et, cursus. 
-                                    Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
-                                    Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
-                                    Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat. 
-                                </p>        
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="heading">
-                                <button data-toggle="collapse" data-toggle-target="card-body-5"><h6>Can I create my own password that is easy for me to remember?</h6></button>
-                                <i className="fa-regular fa-chevron-down"></i>
-                            </div>
-                            <div id="card-body-5" className="card-body d-none">
-                                <p>
-                                    Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
-                                    Viverra aliquam arcu, viverra et, cursus. 
-                                    Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
-                                    Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
-                                    Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.    
-                                </p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="heading">
-                                <button data-toggle="collapse" data-toggle-target="card-body-6"><h6>What happens if I forget or lose my password?</h6></button>
-                                <i className="fa-regular fa-chevron-down"></i>
-                            </div>
-                            <div id="card-body-6" className="card-body d-none">
-                                <p>
-                                    Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
-                                    Viverra aliquam arcu, viverra et, cursus. 
-                                    Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
-                                    Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
-                                    Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.   
-                                </p>
-                            </div>
+                        <AccordionItem
+                            title="Is any of my personal information stored in the App?"
+                            content="Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
+                            Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
+                            Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
+                            Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+                        />
+                          <AccordionItem
+                            title="What formats can I download my transaction history in?"
+                            content="Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
+                            Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
+                            Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
+                            Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+                        />
+                        <AccordionItem
+                            title="Can I schedule future transfers?"
+                            content="Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
+                            Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
+                            Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
+                            Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+                        />
+                        <AccordionItem
+                            title="When can I use Banking App services?"
+                            content="Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
+                            Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
+                            Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
+                            Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+                        />
+                        <AccordionItem
+                            title="Can I create my own password that is easy for me to remember?"
+                            content="Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
+                            Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
+                            Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
+                            Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+                        />
+                        <AccordionItem
+                            title="What happens if I forget or lose my password?"
+                            content="Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. 
+                            Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. 
+                            Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. 
+                            Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+                        />
                         </div>
                     </div>
-                </div>
             </section>
   )
 }
