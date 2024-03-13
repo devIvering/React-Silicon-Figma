fd582e9af601a79d962a1f6545329a8ecadfe5ab
Gustav Ivering <gustav.ivering@live.se>
2024-03-13 19:20:37 +0100

    Added button and carousel to how it works section

--- src/views/sections/Home-page/HowItWorks.jsx
+++ src/views/sections/Home-page/HowItWorks.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import MobileOne from '../../../images/mobile-howdoesitwork_1.svg'
 import MobileTwo from '../../../images/mobile-howdoesitwork_2.svg'
 import MobileThree from '../../../images/mobile-howdoesitwork_3.svg'
+import PreviousButton from '../../../images/icons/prev-btn.svg'
+import NextButton from '../../../images/icons/next-btn_right.svg'
 
 const HowItWorks = () => {
   return (
@@ -10,9 +12,11 @@ const HowItWorks = () => {
             <div className="content">
                 <h1>How Does It Work?</h1>
             <div className="phone-images">
+                <img src={PreviousButton} alt="Previous button" />
                 <img className="phone-one" src={MobileOne} alt="Mobile-phone" />
                 <img className="phone-two" src={MobileTwo} alt="Mobile-phone" />
                 <img className="phone-three" src={MobileThree} alt="Mobile-phone" />
+                <img src={NextButton} alt="Next button" />
             </div>
             <div className="step-2-text">
                 <h2>Step 2. Latest transaction history</h2>
